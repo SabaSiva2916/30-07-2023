bdd83cc128c763487d4d1b7efe9b1f428bda05ec
SabaSiva2916 <sabasiva2916@gmail.com>
2023-07-30 13:59:18 +0530

    List added

--- src/test/java/org/sample/HelloWorld.java
+++ src/test/java/org/sample/HelloWorld.java
@@ -1,8 +1,27 @@
 package org.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloWorld {
     public static void main(String[] args) {
 
 	System.out.println("Hello World");
+	List<String> li = new ArrayList<String>();
+	// Adding value into list
+	li.add("A");
+	li.add("B");
+	li.add("C");
+	li.add("D");
+	li.add("E");
+
+	// Printing the value of list
+	System.out.println(li);
+
+	// Iterating by using for loop
+	for (int i = 0; i < li.size(); i++) {
+	    String string = li.get(i);
+	    System.out.println(string);
+	}
     }
 }
